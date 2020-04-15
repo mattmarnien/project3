fd2693b47c5c23a654a5ecb7e651a7e94fa55048
vadimsusername <vadimburyakovsky@yahoo.com>
2020-04-15 00:53:54 -0400

    added background images to game play area, load cards, user can pick card to play

--- client/src/components/GamePlayPage/index.js
+++ client/src/components/GamePlayPage/index.js
@@ -1,40 +1,137 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css"
-function GamePlay() {
 
+function GamePlay(props) {
+    const [user, setUser] = useState(
+        {
+            cards : [],
+            avatar: "",
+            health: 30,
+            playedCards: []
+        }
+    );
+    const [opponent, setOpponent] = useState(
+        {
+            cards : [],
+            avatar: "",
+            health: 30
+        }
+    );
+
+    const [turn, setTurn] = useState("user");
+    useEffect(() => {
+    
+        setUser(
+            {   ...user,
+                cards: props.userCards,
+                avatar: props.userAvatar
+            }
+        );
+
+        setOpponent(
+            {   ...opponent,
+                cards: props.opponentCards,
+                avatar: props.opponentAvatar
+            }
+        );
+
+        
+      }, []);
+    
+    function cardSelect(event) {
+       /*  if(turn === "user"){ */
+            console.log(event.target.id);
+             var card  = user.cards.filter(card => {
+            console.log(card._id);
+            return card._id === parseInt(event.target.id);
+            }
+            );
+             console.log(card);
+        
+              var cardHand = user.cards.filter(card => {
+            return card._id !== parseInt(event.target.id);
+           });
+
+           setUser({
+                ...user,
+                playedCards :[...user.playedCards,card[0]],
+                cards: cardHand
+            }); 
+                 console.log(card[0].attack);
+            setOpponent ({
+                ...opponent,
+                health: opponent.health - card[0].attack
+            })
+            /* setTurn("opponent");
+            opponentMove(); */
+       /*  } */
+    }
+
+    function opponentMove() {
+        /* ... */
+    }
+   
+    
     return (
+
         <>
             <div className="container  gamearea">
-                <div className="row opponentRow1 grey lighten-2">
-                   <div class="opponentHand  blue-grey lighten-4">
-                        Opponent Hand
+                <div className="row opponentRow1 ">
+                   <div class="opponentHand  ">
+                        <div className="hp"> 
+                            Health
+                            <br />
+                            {opponent.health}
+                        </div>
+                        {opponent.avatar === "" ? "" :  <img className="opponentAvatar" src={require("../assets/images/" + opponent.avatar)} alt="Opponent Avatar"></img>} 
+                        {opponent.cards.map(card => {
+                            return      (                            
+                                <img className="opponentCard" src={require("../assets/images/" + card.image)} alt={card.name}></img>
+                            )
+                        })}
                    </div>
                 </div>
                 <div className="row opponentRow2 ">
                     <div className="col s2 ">
                        
                     </div>
-                    <div className="col s10 opponentPlayedCardsArea light-blue lighten-2">
-                         <div class="opponentPlayedCards purple lighten-2">
-                           Opponent Played Cards Area
+                    <div className="col s10 opponentPlayedCardsArea">
+                         <div class="opponentPlayedCards ">
+                           
                         </div>
                     </div>
                 </div>
-                <div className="row playerRow1 lime lighten-5">
-                    <div className="col s2 resourceArea red lighten-4">
-                         Resource Area
+                <div className="row userRow1 ">
+                    <div className="col s2 resourceArea">
+                       
                     </div>
-                    <div className="col s10 userPlayedCardsArea light-blue lighten-5">
-                         <div class="userPlayedCards purple lighten-4">
-                        User Played Cards Area
+                    <div className="col s10 userPlayedCardsArea ">
+                         <div class="userPlayedCards ">
+                            {user.playedCards.map(card => {
+                                return      (                            
+                                    <img className="userCard" id={card._id} src={require("../assets/images/" + card.image)} alt={card.name} onClick={cardSelect}></img>
+                                )
+                            })}
                         </div>
                     </div>
                  </div>
-                 <div className="row playerRow2 green lighten-5">
-                    <div className="col s12 playerHandArea">
-                        <div class="playerHand  blue-grey lighten-3">
-                            Player Hand
-                         </div>
+                 <div className="row userRow2">
+                    <div className="col s12 userHandArea">
+                        <div class="userHand  ">
+     
+                            <div className="hp"> 
+                                Health
+                                <br />
+                                {user.health}
+                            </div>
+                           
+                            {user.avatar === "" ? "" :  <img className="userAvatar" src={require("../assets/images/" + user.avatar)} alt="User Avatar"></img>}
+                            {user.cards.map(card => {
+                                return      (                            
+                                    <img className="userCard" id={card._id} src={require("../assets/images/" + card.image)} alt={card.name} onClick={cardSelect}></img>
+                                )
+                            })}
+                        </div>
                     </div>
                 </div> 
             </div>
@@ -45,41 +142,3 @@ function GamePlay() {
 }
 
 export default GamePlay;
-
-                    
-                        /* Alternative player area layout */
-                {/* <div className="col s2 lime lighten-5 row2col1">
-                        Resource Area
-                   </div>
-                   <div className="col s10 blue lighten-5 row2col2">
-
-                   </div> */}
-
-                  /*  <div className="col s12">
-                        <div className="row playerRow1 lime lighten-5">
-                            <div className="col s2 resourceArea red lighten-4">
-                                    Resource Area
-                            </div>
-                            <div className="col s10 playedCardsArea light-blue lighten-5">
-                                <div class="playedCards purple lighten-4">
-                                    Played Cards Area
-                                </div>
-                            </div>
-                        </div>
-                        <div className="row playerRow2 green lighten-5">
-                            <div className="col s12 playerHandArea">
-                                <div class="playerHand  blue-grey lighten-3">
-                                    Player Hand
-                                </div>
-                            </div>
-                        </div>
-                    </div> */
-
-                  /*   <div className="container  gamearea">
-                    <div className="row opponentArea grey lighten-2">
-                        Opponent Area
-                    </div>
-                    <div className="row playerArea brown lighten-4">
-                         
-                    </div>
-                </div> */
